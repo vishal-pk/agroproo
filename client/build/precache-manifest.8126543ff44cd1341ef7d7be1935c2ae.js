4e636c001e64ccf5e9e6f4f51e03ef8a0e457644
vishal <pk.vishal24@gmail.com>
2020-03-08 11:11:19 +0530

    test

---
+++ client/build/precache-manifest.8126543ff44cd1341ef7d7be1935c2ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92235cb77e26f613beb5306ec92765b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "058b7332f9bd46892e5c",
+    "url": "/static/css/main.1fc450e0.chunk.css"
+  },
+  {
+    "revision": "19768f6f0532b08e068e",
+    "url": "/static/js/2.a9c20376.chunk.js"
+  },
+  {
+    "revision": "507a7370b52a72b5f39c2fecbeb4dc40",
+    "url": "/static/js/2.a9c20376.chunk.js.LICENSE"
+  },
+  {
+    "revision": "058b7332f9bd46892e5c",
+    "url": "/static/js/main.b668672e.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
